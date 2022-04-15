803c1699eac8e22a18953e4a1e1d1b8924b465e4
Tsenguun <tsenguun@Tsenguuns-MacBook-Pro.local>
2022-04-15 20:59:48 +0200

    Add all my files

---
+++ screens/Details.js
@@ -0,0 +1,24 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
+
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+  FocusedStatusBar,
+} from "../components";
+
+const Details = ({ route, navigation }) => {
+  console.log(route);
+  return <Text>Detailsr</Text>;
+};
+
+export default Details;
